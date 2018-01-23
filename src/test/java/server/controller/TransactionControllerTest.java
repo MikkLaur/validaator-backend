5644637204f440766f185dd22f73ad20e524667c
Mikk Laur <mikklaur5@gmail.com>
2018-01-23 16:22:04 +0200

    Clean slate to set up testing.

--- src/test/java/server/controller/TransactionControllerTest.java
+++ src/test/java/server/controller/TransactionControllerTest.java
@@ -8,15 +8,4 @@ import java.sql.Date;
 import static org.junit.Assert.*;
 
 public class TransactionControllerTest {
-
-    @Test
-    public void buyTicket() {
-        DatabaseWrapper.resetDatabase();
-        long userid = DatabaseWrapper.insertUser("Mikk Laur", "39505312733", Date.valueOf("1995-05-31"));
-        long stopId = DatabaseWrapper.insertStop("Jaama");
-
-        TransactionController tc = new TransactionController();
-
-        assertEquals(1, tc.buyTicket(String.valueOf(userid), String.valueOf(stopId)));
-    }
 }
---
+++ src/test/java/server/database/DatabaseWrapperTest.java
@@ -0,0 +1,11 @@
+package server.database;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.lang.reflect.*;
+
+import static org.junit.Assert.*;
+
+public class DatabaseWrapperTest {
+}
