5a8c831b738e6b39fc662d88c36412164f2b352b
Mikk Laur <mikklaur5@gmail.com>
2018-01-26 13:08:36 +0200

    Add hacked tests for StopController and UserController

--- src/test/java/server/controller/StopControllerTest.java
+++ src/test/java/server/controller/StopControllerTest.java
@@ -1,4 +1,23 @@
 package server.controller;
 
+import org.json.JSONArray;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class StopControllerTest {
+    private StopController sc = new StopController();
+
+    @Test
+    public void getAllJSONTest() {
+        JSONArray jsonArray = new JSONArray(sc.getAllJSON());
+        /* Reminder to myself. This is a bad way of testing.
+         * Basically, i know there will be 7 stops on the db during test task.
+         * Each test should be cleaning up after itself
+         * TODO: Implement something better. */
+        Assert.assertEquals(7, jsonArray.length());
+
+        Assert.assertNotEquals(0, jsonArray.length());
+
+        // Hopefully this helps me get through the refactor
+    }
 }
--- src/test/java/server/controller/UserControllerTest.java
+++ src/test/java/server/controller/UserControllerTest.java
@@ -1,9 +1,32 @@
 package server.controller;
 
+import org.json.JSONArray;
+import org.junit.Assert;
 import org.junit.Test;
-import server.database.DatabaseWrapper;
-
-import static org.junit.Assert.*;
 
 public class UserControllerTest {
+    private UserController uc = new UserController();
+
+    @Test
+    public void registerUserTest() {
+        String name = "im a name";
+        String personalId = "im a personal id";
+        String date = "1990-09-23";
+        Assert.assertNotEquals(-1, uc.registerUser(name, personalId, date));
+        Assert.assertEquals(-1, uc.registerUser(name, personalId, date));
+    }
+
+    @Test
+    public void getAllJSONTest() {
+        JSONArray jsonArray = new JSONArray(uc.getAllJSON());
+        /* Reminder to myself. This is a bad way of testing.
+         * Basically, i know there will be 11 users on the db during test task.
+         * Each test should be cleaning up after itself
+         * TODO: Implement something better. */
+        Assert.assertEquals(11, jsonArray.length());
+
+        Assert.assertNotEquals(0, jsonArray.length());
+
+        // Hopefully this helps me get through the refactor
+    }
 }
